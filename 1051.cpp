d91dbc76bf06004338bdde6474fd7936fb00bb0a
Baramini <145976578+Baramini@users.noreply.github.com>
2024-07-10 01:10:05 +0900

    jeongyong

--- 1051.cpp
+++ 1051.cpp
@@ -1,17 +1,37 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
-    int N, M, sqa = 1, min, edge = 2;
+    int N, M, maxE, maxS = 1;
     cin >> N >> M;
-    int** rec = new int*[N];
-    for (int i = 0; i < N; i++) rec[i] = new int[M];
-    min = (N > M) ? M : N;
-    while (min >= edge) {
-        for (int i = 0; i < N - edge; i++) {
-            for (int j = 0; j < M - edge; j++) {
-                if ()
+    maxE = (N < M) ? N : M;
+
+    string s;
+    int **arr = new int*[N];
+    for (int i = 0; i < N; i++) arr[i] = new int[M];
+    for (int j = 0; j < N; j++) {
+        cin >> s;
+        for (int k = 0; k < M; k++) {
+            arr[j][k] = (int)(s[k] - '0');
+        }
+    }
+
+    for (int i = 1; i < maxE; i++) {
+        for (int j = 0; j < N - i; j++) {
+            if ((i + 1) * (i + 1) == maxS) break;
+            for (int k = 0; k < M - i; k++) {
+                if ((i + 1) * (i + 1) == maxS) break;
+                int a = arr[j][k];
+                int b = arr[j][k + i];
+                int c = arr[j + i][k];
+                int d = arr[j + i][k + i];
+                if (a == b && c == d && a == c) maxS = (i + 1) * (i + 1);
             }
         }
     }
+    cout << maxS;
+
+    for (int i = 0; i < N; i++) delete[] arr[i];
+    delete[] arr;
 }
