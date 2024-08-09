4eabd1050cfe1a53eb642cbce96d8a2ce117ade5
Baramini <145976578+Baramini@users.noreply.github.com>
2024-08-09 17:50:23 +0900

    jeongyong

--- 1059.cpp
+++ 1059.cpp
@@ -1,40 +1,34 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 using namespace std;
 
 int main() {
-    int L, n, ans;
+    int L, n, ans = 0;
+    int low = 1001, A;
+    int high = -1001, B;
+
     cin >> L;
     vector<int> v;
     for (int i = 0; i < L; i++) {
         cin >> n;
         v.push_back(n);
     }
-    sort(v.begin(), v.end());
     cin >> n;
 
-    vector<int>::iterator i = v.begin();
-    if (L == 1) ans = 0;
-    else if (*i > n) {
-        
-    }
-    else {
-        for (i = v.begin(); i != (v.end() - 2); i++) {
-            if ((*i == n) || ((*i < n && *(i + 1) > n))) break;
+    for (vector<int>::iterator i = v.begin(); i != v.end(); i++) {
+        int j = n - *i;
+        if (j > 0) {
+            if (low > j) low = j;
         }
-        if (*i == n) ans = 0;
-        else if (*(i + 1) - *i < 3) ans = 0;
         else {
-            int a, b;
-            a = *i++; b = *i - 1;
-            ans = 0;
-            for (int j = a + 1; j <= n; j++) {
-                ans += (b - j);
-                if (n - j > 1) ans -= (n - j - 1);
-            }
+            if (high < j) high = j;
         }
     }
-    
+    if (low == 1001) A = 0;
+    else A = n - low;
+    B = n + (-high);
+
+    ans = (n - A) * (B - n);
+    ans = (ans > 0) ? ans - 1 : ans; 
     cout << ans;
 }
